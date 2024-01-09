f0f1d3ccada61093a199b2b81c77f484a1d80a78
Anastasiia <humenyuknastia.9616@gmail.com>
2024-01-09 20:59:23 +0100

    implement week1 task

---
+++ script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const quizQuestions = [];
+    const quizForm = document.getElementById("quiz-form");
+    const options = quizForm.querySelectorAll('input[type="text"]');
+    const select = document.getElementById("correct-answer");
+
+    quizForm.addEventListener("submit", function(event){
+        event.preventDefault();
+        const newQuestionText = document.getElementById("questionInput").value;
+        const correctIndex = parseInt(select.value) - 1;
+        options.forEach((option,index)=>{
+            option.style.backgroundColor = index === correctIndex ? "green" : "red";
+        });
+
+        const newOption = [];
+        for (let i = 0; i < options.length; i++) {
+            newOption.push({
+                text: options[i].value,
+                isCorrect: i === correctIndex,
+            });
+        }
+
+        if (newQuestionText.length <= 140){
+            const newQuestion ={
+                question: newQuestionText,
+                options: newOption,
+            };
+            quizQuestions.push(newQuestion);
+            
+        } else{
+            alert("Question must be less than 140 characters");
+        }
+    });
+});
+
